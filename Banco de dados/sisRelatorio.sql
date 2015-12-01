91bb96b8bfe8a8178916e4bcb6cc869615dbdd27
Larissa Menandro <la.menandro@gmail.com>
2015-12-01 16:24:46 -0200

    script de criação do schema relatorio

---
+++ Banco de dados/sisRelatorio.sql
@@ -0,0 +1,28 @@
+	begin transaction;
+	
+	/*==============================================================*/
+	/* dbms name:      postgresql 9.x                               */
+	/* created on:     01/12/2015 		                            */
+	/*==============================================================*/
+
+	begin transaction;
+	set statement_timeout = 0;
+	set client_encoding = 'utf8';
+	set standard_conforming_strings = on;
+	set check_function_bodies = false;
+	set client_min_messages = warning;
+
+
+	-- -----------------------------------------------------------------------
+	-- --------------- s c h e m a   G E O  ------------------
+	-- -----------------------------------------------------------------------
+	--
+	-- criação do schema
+	create schema relatorio;
+	alter schema relatorio owner to postgres;
+	set search_path = relatorio, pg_catalog;
+	set default_tablespace = '';
+	set default_with_oids = false;
+	
+
+rollback transaction;
